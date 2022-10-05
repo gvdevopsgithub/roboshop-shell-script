a66368edf020f5ee8a2c3addf71ab90176488246
gv <gvdevops00@gmail.com>
2022-10-05 11:02:29 +0530

    dir statement

--- file.sh
+++ file.sh
@@ -2,9 +2,16 @@
 
 echo " Enter file Name: "
 read File_name
-if [ -f $File_name ]
+#if [ -f $File_name ]
+#then
+#  echo " File $File_name found "
+#  else
+#    echo "File $File_name not found "
+#    fi
+
+if [ -d $File_name ]
 then
-  echo " File $File_name found "
+  echo " dir $File_name found "
   else
-    echo "File $File_name not found "
+    echo "dir $File_name not found "
     fi
