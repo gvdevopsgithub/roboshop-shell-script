535ccdd172ae19a2487c921153f9e5f11b0050a3
gv <gvdevops00@gmail.com>
2022-10-06 00:57:02 +0530

    for file

--- for.sh
+++ for.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-for i in *.pdf; do
-  size=$(du -h "$i")
-  echo "File: $size"
-  done
+#for i in *.pdf; do
+#  size=$(du -h "$i")
+#  echo "File: $size"
+#  done
+
+
+value=0
+for i in /root/scripts/*.pdf; do
+  DUU=$(du -h "$i" | awk '{print $1}')
+  if [ $DUU == $value ]
+  then
+    echo " 0KB files $i "
+    mv -f $i /root/test
+    fi
+    done
