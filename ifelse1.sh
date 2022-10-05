f39b44ef6f8a9ba3e9eb7bbe563e89317f1d338c
gv <gvdevops00@gmail.com>
2022-10-05 10:43:29 +0530

    push

--- ifelse1.sh
+++ ifelse1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Compare two numbers
-
-echo "Enter a Value: "
-read a
-echo "Enter b Value: "
-read b
+## Compare two numbers
+#
+#echo "Enter a Value: "
+#read a
+#echo "Enter b Value: "
+#read b
 
 
 #if [ $a == $b ]
@@ -15,9 +15,32 @@ read b
 #    echo " a is not equal to b "
 #    fi
 
-if [ $a -eq $b ]
-then
-  echo " a is Greaterthan or equal to  b "
-  else
-    echo " a is lessthan or equal to b "
-    fi
+
+#if [ $a -eq $b ]
+#then
+#  echo " a is Greaterthan or equal to  b "
+#  else
+#    echo " a is lessthan or equal to b "
+#    fi
+
+
+
+#Purpose: Find Biggest number among 4 Digits
+echo -e "Please Enter a Value: \c"
+read -r a
+echo -e "Please Enter b Value: \c"
+read -r b
+echo -e "Please Enter c Value: \c"
+read -r c
+echo -e "Please Enter d Value: \c"
+read -r d
+
+if [ $a -gt $b -a $a -gt $c -a $a -gt $d ]; then
+echo "$a a is big"
+elif [ $b -gt $c -a $b -gt $d ]; then
+  echo "$b b is big"
+  elif [ $c -gt $d ]; then
+    echo "$c c is big"
+    else
+      echo "$d d is big"
+      fi 
