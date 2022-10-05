805357e14d8b9102fb023ec3d49171976b4952b5
gv <gvdevops00@gmail.com>
2022-10-05 19:10:45 +0530

    marks statement

--- 10th.sh
+++ 10th.sh
@@ -5,7 +5,17 @@ read SSC
 echo "Enter Inter Percentage"
 read Inter
 
-if [ $SSC -ge 70 -a $Inter -ge 60 ]
+#
+#if [ $SSC -ge 70 -a $Inter -ge 60 ]
+#then
+#  echo "Candidate eligible"
+#  else
+#    echo "Candidate not eligible"
+#    fi
+#
+
+
+if [ $SSC -ge 70 -o $Inter -ge 60 ]
 then
   echo "Candidate eligible"
   else
