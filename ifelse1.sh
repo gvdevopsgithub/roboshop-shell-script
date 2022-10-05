fe90d3359835553bc1878979e50d9f36a6a99d63
gv <gvdevops00@gmail.com>
2022-10-05 10:18:35 +0530

    push

--- ifelse1.sh
+++ ifelse1.sh
@@ -8,9 +8,16 @@ echo "Enter b Value: "
 read b
 
 
-if [ $a == $b ]
+#if [ $a == $b ]
+#then
+#  echo " a is equal to b "
+#  else
+#    echo " a is not equal to b "
+#    fi
+
+if [ $a -eq $b ]
 then
-  echo " a is equal to b "
+  echo " a is Greaterthan or equal to  b "
   else
-    echo " a is not equal to b "
+    echo " a is lessthan or equal to b "
     fi
