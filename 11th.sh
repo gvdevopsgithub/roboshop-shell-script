3451d4671a849491007e8eaabd690b0355288b4b
gv <gvdevops00@gmail.com>
2022-10-05 19:52:47 +0530

    arthimatic statement

--- 11th.sh
+++ 11th.sh
@@ -5,7 +5,10 @@ read  num1
 echo "Enter 2nd num: "
 read num2
 
-echo "Addition :" $(( $num1 + $num2 ))
-echo "Substraction :" $(( $num1 - $num2 ))
-echo "Multiplication :" $(( $num1 * $num2 ))
-echo "Division :" $(( $num1 / $num2 ))
+#
+#echo "Addition :" $(( $num1 + $num2 ))
+#echo "Substraction :" $(( $num1 - $num2 ))
+#echo "Multiplication :" $(( $num1 * $num2 ))
+#echo "Division :" $(( $num1 / $num2 ))
+
+echo "Scale=2;$num1/$num2" | bc
