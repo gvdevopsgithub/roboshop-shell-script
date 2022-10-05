bd90b654b985a9738c1645d1e9c31d53c22dfc30
gv <gvdevops00@gmail.com>
2022-10-06 00:43:51 +0530

    13th file

--- 13.sh
+++ 13.sh
@@ -3,7 +3,12 @@
 #for i in 1 2 3 4 5 ; do
 #  echo "Number $i"
 #  done
+#
+#for i in {1..10..2} ; do
+#  echo "Number $i"
+#  done
 
-for i in {1..10..2} ; do
+
+for ((i=0; i<10 ; i++)) ; do
   echo "Number $i"
   done
