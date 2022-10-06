265fd3ad01640f0af965e2cf08231ef93c8322ac
gv <gvdevops00@gmail.com>
2022-10-06 10:47:37 +0530

    dir and files

--- firstscript.sh
+++ firstscript.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-mkdir 7hills
-echo "your directory 7hills is created successfully"
-touch 7hills/test{1..20}.txt
-echo "your files from test 1 to 20 files created"
-ls -ltr 7hills/*
-echo "above are the files"
-rm -rf 7hills/*
-echo "your files are successfully deleted"
+#mkdir 7hills
+#echo "your directory 7hills is created successfully"
+#touch 7hills/test{1..20}.txt
+#echo "your files from test 1 to 20 files created"
+#ls -ltr 7hills/*
+#echo "above are the files"
+#rm -rf 7hills/*
+#echo "your files are successfully deleted"
+#
 
+
+mkdir venky
+echo "Your Directory venky Created Successfully"
+touch venky/normal{1..20}.txt
+echo "your files from normal.txt 1 to 20 files Created"
+ls -ltr venky/*
+echo "Above are the Files"
+rm -rf venky/*
+echo "Your files are Successfully Deleted"
