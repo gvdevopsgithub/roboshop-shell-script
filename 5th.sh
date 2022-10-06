37c9c878280810b50c91013bb0abbf065d08a4bd
gv <gvdevops00@gmail.com>
2022-10-06 11:58:35 +0530

    Arguments command

--- 5th.sh
+++ 5th.sh
@@ -2,13 +2,13 @@
 
 ### Print total arguments their values
 
-echo "Total Arguments:" $#
-echo "All Arguments values:" $@
-
-### Command arguments can be accessed as
-
-echo "First->" $1
-echo "Second->" $2
+#echo "Total Arguments:" $#
+#echo "All Arguments values:" $@
+#
+#### Command arguments can be accessed as
+#
+#echo "First->" $1
+#echo "Second->" $2
 
 
 echo "Total arguments:" $#
