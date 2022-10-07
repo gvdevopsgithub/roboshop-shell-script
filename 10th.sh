4caaa1ce90e426cc0b47c6606a3650b785195db0
gv <gvdevops00@gmail.com>
2022-10-07 09:03:27 +0530

    arthimatic statement

--- 10th.sh
+++ 10th.sh
@@ -1,39 +1,26 @@
-#!/bin/bash
-
-#echo "Enter SSC Percentage: "
-#read SSC
-#echo "Enter Inter Percentage: "
-#read Inter
-
-#
-#if [ $SSC -ge 70 -a $Inter -ge 60 ]
-#then
-#  echo "Candidate eligible"
-#  else
-#    echo "Candidate not eligible"
-#    fi
-#
-
-
-##if [ $SSC -ge 70 -a $Inter -ge 60 ]
-#if [ $SSC -ge 70 ] & [ $Inter -ge 60 ]
-#then
-#  echo "Candidate eligible"
-#  else
-#    echo "Candidate not eligible"
-#    fi
-
-
-
+!/bin/bash
 
 echo "Enter SSC Percentage: "
 read SSC
-echo "Enter INTER Percentage: "
-read INTER
+echo "Enter Inter Percentage: "
+read Inter
 
-if [ $SSC -ge 70 ] || [ $INTER -ge 60 ]
+
+if [ $SSC -ge 70 -a $Inter -ge 60 ]
 then
   echo "Candidate eligible"
   else
     echo "Candidate not eligible"
     fi
+
+
+
+#if [ $SSC -ge 70 -a $Inter -ge 60 ]
+if [ $SSC -ge 70 ] & [ $Inter -ge 60 ]
+then
+  echo "Candidate eligible"
+  else
+    echo "Candidate not eligible"
+    fi
+
+
--- 11th.sh
+++ 11th.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-echo "Enter 1st num: "
-read  num1
-echo "Enter 2nd num: "
-read num2
+#
+#echo "Enter 1st num: "
+#read  num1
+#echo "Enter 2nd num: "
+#read num2
 
 #
 #echo "Addition :" $(( $num1 + $num2 ))
@@ -11,4 +11,14 @@ read num2
 #echo "Multiplication :" $(( $num1 * $num2 ))
 #echo "Division :" $(( $num1 / $num2 ))
 
-echo "scale=2;$num1/$num2" | bc
+#echo "scale=2;$num1/$num2" | bc
+
+echo "Enter 1st number: "
+read num1
+echo "Enter 2nd number: "
+read num2
+
+echo "Addition: " $(( $num1 + $num2 ))
+echo "Substraction: " $(( $num1 - $num2 ))
+echo "Multiplication: " $(( $num1 * $num2 ))
+echo "Division: " $(( $num1 / $num2 ))
