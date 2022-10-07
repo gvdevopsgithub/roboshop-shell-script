bde92c24888d4fb8da581316a2f11904e3fd6f95
gv <gvdevops00@gmail.com>
2022-10-07 08:45:00 +0530

    percentage statement

--- 10th.sh
+++ 10th.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Enter SSC Percentage"
-read SSC
-echo "Enter Inter Percentage"
-read Inter
+#echo "Enter SSC Percentage: "
+#read SSC
+#echo "Enter Inter Percentage: "
+#read Inter
 
 #
 #if [ $SSC -ge 70 -a $Inter -ge 60 ]
@@ -15,8 +15,23 @@ read Inter
 #
 
 
-#if [ $SSC -ge 70 -a $Inter -ge 60 ]
-if [ $SSC -ge 70 ] & [ $Inter -ge 60 ]
+##if [ $SSC -ge 70 -a $Inter -ge 60 ]
+#if [ $SSC -ge 70 ] & [ $Inter -ge 60 ]
+#then
+#  echo "Candidate eligible"
+#  else
+#    echo "Candidate not eligible"
+#    fi
+
+
+
+
+echo "Enter SSC Percentage: "
+read SSC
+echo "Enter INTER Percentage: "
+read INTER
+
+if [ $SSC -ge 70 -a $INTER -ge 60 ]
 then
   echo "Candidate eligible"
   else
