d69f1aeed5bcd84528ad41f93c81d53f953dda9c
gv <gvdevops00@gmail.com>
2022-10-07 09:14:13 +0530

    arthimatic statement

--- 11th.sh
+++ 11th.sh
@@ -1,29 +1,14 @@
-#!/bin/bash
-#
-#echo "Enter 1st num: "
-#read  num1
-#echo "Enter 2nd num: "
-#read num2
+!/bin/bash
 
-#
-#echo "Addition :" $(( $num1 + $num2 ))
-#echo "Substraction :" $(( $num1 - $num2 ))
-#echo "Multiplication :" $(( $num1 * $num2 ))
-#echo "Division :" $(( $num1 / $num2 ))
-
-#echo "scale=2;$num1/$num2" | bc
-
-echo "Enter 1st number: "
-read num1
-echo "Enter 2nd number: "
+echo "Enter 1st num: "
+read  num1
+echo "Enter 2nd num: "
 read num2
 
-#echo "Addition: " $(( $num1 + $num2 ))
-#echo "Substraction: " $(( $num1 - $num2 ))
-#echo "Multiplication: " $(( $num1 * $num2 ))
-#echo "Division: " $(( $num1 / $num2 ))
 
-echo "Addition: " "scale=2;$num1+$num2" | bc
-echo "Substraction: " "scale=2;$num1-$num2" | bc
-echo "Multiplication: " "scale=2;$num1*$num2" | bc
-echo "Division: " "scale=2;$num1/$num2" | bc
+echo "Addition :" $(( $num1 + $num2 ))
+echo "Substraction :" $(( $num1 - $num2 ))
+echo "Multiplication :" $(( $num1 * $num2 ))
+echo "Division :" $(( $num1 / $num2 ))
+
+echo "scale=2;$num1/$num2" | bc
--- 12th.sh
+++ 12th.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+#read -p "Enter numeric value: " EnteredValue
+#
+#if [ $EnteredValue -gt 90 ]
+#then
+#  echo "Entered Value is greater than 90"
+#
+#  elif [ $EnteredValue -ge 70 ]
+#  then
+#    echo "Entered Value is greater than 70"
+#    else
+#      echo "Entered Value is less than 70"
+#      fi
+#
+
 read -p "Enter numeric value: " EnteredValue
 
-if [ $EnteredValue -gt 90 ]
+if [$EnteredValue -gt 90 ]
 then
   echo "Entered Value is greater than 90"
-
-  elif [ $EnteredValue -ge 70 ]
-  then
-    echo "Entered Value is greater than 70"
-    else
-      echo "Entered Value is less than 70"
-      fi
-
+  fi
