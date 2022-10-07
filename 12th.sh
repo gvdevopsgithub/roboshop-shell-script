c627a1fa1fd1a0ab1f24f4b79d43fe22073beb20
gv <gvdevops00@gmail.com>
2022-10-07 09:27:23 +0530

    arthimatic statement

--- 12th.sh
+++ 12th.sh
@@ -19,6 +19,9 @@ read -p "Enter numeric value: " EnteredValue
 if [ $EnteredValue -gt 90 ]
 then
   echo "Entered Value is greater than 90"
-  else
-    echo "Entered Value is less than 90"
+  elif [ $EnteredValue -ge 70 ]
+  then
+    echo "Entered Value is greater than 70"
+    else
+      echo "Entered Value is less than 70"
   fi
